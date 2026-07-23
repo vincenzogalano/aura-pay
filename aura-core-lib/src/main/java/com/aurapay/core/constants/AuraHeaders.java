f986611b0f1d0edcaa505b019366fb1bf2b1a93c
VincenzoGalano <vincenzogalano@hotmail.com>
2026-07-23 19:24:23 +0200

    refactor(core): update AuraHeaders with AUTHORIZATION and API_KEY constants

--- aura-core-lib/src/main/java/com/aurapay/core/constants/AuraHeaders.java
+++ aura-core-lib/src/main/java/com/aurapay/core/constants/AuraHeaders.java
@@ -7,6 +7,8 @@ public final class AuraHeaders {
 
     public static final String CORRELATION_ID = "X-Correlation-ID";
     public static final String IDEMPOTENCY_KEY = "Idempotency-Key";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String API_KEY = "X-Api-Key";
 
     private AuraHeaders() {
         // Utility class
