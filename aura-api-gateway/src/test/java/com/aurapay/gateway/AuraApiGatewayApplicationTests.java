33af5cdafe37c7d19241a9671f169d2a5b166f7f
VincenzoGalano <vincenzogalano@hotmail.com>
2026-07-23 19:34:51 +0200

    refactor(gateway): add ActiveProfiles test annotation to AuraApiGatewayApplicationTests

--- aura-api-gateway/src/test/java/com/aurapay/gateway/AuraApiGatewayApplicationTests.java
+++ aura-api-gateway/src/test/java/com/aurapay/gateway/AuraApiGatewayApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@ActiveProfiles("test")
 class AuraApiGatewayApplicationTests {
 
     @Autowired
